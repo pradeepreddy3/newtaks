1cb5b2754e39f277683cdecd8080dd829c57524a
pradeepreddy3 <pradeepreddy3@gmail.com>
2012-12-17 15:58:12 +0530

    Update pradeepreddy_task.sh

--- pradeepreddy_task.sh
+++ pradeepreddy_task.sh
@@ -1,37 +1,77 @@
 #!/bin/bash
 LOG_FILE="pradeep_reddy_task.log"
 
-case $1 in
-        --change_password )
-                echo change password
-                if [ "$#" -eq 3 ] # condition to check if number of parameters passed is 3.
-                then
-                        `pwd`/change_password.sh "$2" "$3" >>$LOG_FILE 2>&1
-                else
-                        echo "Enter Username and Password" >>$LOG_FILE
-                fi;;
-        --disk_space )
-                echo disk space
-                df -h;; # command to show dish usage
-        --login )
-                echo Loging to another box
-                if [ "$#" -eq 4 ]; then
-                        `pwd`/login_ssh.sh $2 $3 $4
-                else
-                        echo "Enter corrent values " >>$LOG_FILE
-                fi;;
-        --service_running )
-                ps -aux ;;
-        --ports_open )
-                echo Ports which are open are
-                ss -l;; ## command to list all open ports
-        --java_apps )
-                echo "Java appas"
-                ps -aux | grep java;;
-        --kill_app )
-                echo "kill app"
-                killall $2
-                ;;
-        * )
-                echo "Usage pradeepreddy_task.sh --[change_password/disk_space/login/ports_open/java_apps/kill_app]"  ;;
-esac
+while [ 1 ]
+do
+        echo "1. Change Password \n "
+        echo "2. Disk Pace \n"
+        echo "3. Login \n"
+        echo "4. Service Running \n"
+        echo "5. Ports Open \n"
+        echo "6. Java Appas \n"
+        echo "7. Kill app \n"
+        echo "8. to exit\n"
+        echo -e "Enter Option [ 1 - 8 ]:"
+        read Choice
+        case $Choice in
+                1)
+                        echo "Enter Username to change"
+                        read Username
+                        if [ "$Username" ]; then
+                                echo "Enter New Password "
+                                read New_Password
+                                if [ "$New_Password" ]; then
+                                        `pwd`/change_password.sh "$Username" "$New_Password" >>$LOG_FILE 2>&1
+                                else
+                                        echo "Invalid Password"
+                                fi
+
+                        else
+                                echo "Invalid Username"
+                        fi;;
+                2)
+                        echo "Disk Space Utilization"
+                        df -h;; # command to show dish usage
+                3)
+                        echo "Login to another box"
+                        echo "Enter IP address of box to login in"
+                        read IP
+                        if [ "$IP" ]; then
+                                echo "Enter Username to be login in "
+                                read Username
+                                if [ "Username" ]; then
+                                        echo "Enter Password "
+                                        read Password
+                                        if [ "$Password" ];
+                                        then
+                                                `pwd`/login_ssh.sh $Username $IP $Password
+                                        else
+                                                echo "Invalid Password"
+                                        fi
+                                else
+                                        echo "Invalid Username"
+                                fi
+                        else
+                                echo "Invalid IP"
+                        fi;;
+                4)
+                        echo "Service running are "
+                        ps -aux ;;
+                5)
+                        echo "Ports Open are "
+                        ss -l;; ## command to list all open ports
+                6)
+                        echo "JAVA Apps running are "
+                        ps -aux | grep java;;
+                7)
+                        echo "Enter app to be killed"
+                        read app_to_kill
+                        if [ "$app_to_kill" ]; then
+                                killall $app_to_kill
+                        else
+                                echo "Invalid entry"
+                        fi;;
+                8)
+                        exit ;;
+        esac
+done
